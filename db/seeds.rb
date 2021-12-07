c276322b1b78dd2c852c6816e37334047cc9d632
belladwir <belladwirh@gmail.com>
2021-12-07 15:04:39 +0700

    signin signup feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = Pasien.create([
+    {
+    nama_depan: "bella", nama_belakang: "dwi", email: "bella@gmail.com",  password: "manggo", umur: 20, jenis_kelamin: "P", no_hp: "081234567890", foto: "bella/url"
+    },
+    {
+        nama_depan: "izsa", nama_belakang: "zsa", email: "izsa@gmail.com",  password: "pineapple", umur: 24, jenis_kelamin: "P", no_hp: "081298765432", foto: "izsa/url"
+    },
+    {
+        nama_depan: "bagas", nama_belakang: "gas", email: "bagas@gmail.com",  password: "blueberry", umur: 26, jenis_kelamin: "L", no_hp: "088901234567", foto: "bagas/url"
+    },
+    {
+        nama_depan: "danny", nama_belakang: "nny", email: "danny@gmail.com",  password: "peanut", umur: 21, jenis_kelamin: "L", no_hp: "086785432467", foto: "danny/url"
+    }])
