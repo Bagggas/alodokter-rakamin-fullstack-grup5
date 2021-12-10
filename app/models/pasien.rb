997a998343b89d4d189803632b0d29675be37f93
Bagggas <abbbagas@gmail.com>
2021-12-10 17:14:25 +0700

    Revert "[WIP] Add Reset password for user"
    This reverts commit ecb0106e90c47669852edd1722db0ea8bddbe34f.

--- app/models/pasien.rb
+++ app/models/pasien.rb
@@ -13,28 +13,5 @@ class Pasien < ApplicationRecord
   
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   
-  # password reset
   
-  def generate_password_token!
-    self.reset_password_token = generate_token
-    self.reset_password_sent_at = Time.now.utc
-    save!
-  end
-   
-  def password_token_valid?
-    (self.reset_password_sent_at + 4.hours) > Time.now.utc
-  end
-  
-  def reset_password!(password)
-    self.reset_password_token = nil
-    self.password = password
-    save!
-  end
-   
-  private
-  
-  def generate_token
-    SecureRandom.hex(10)
-  end
-
 end
--- db/migrate/20211210042200_add_password_reset_columns_to_pasien.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordResetColumnsToPasien < ActiveRecord::Migration[6.1]
-  def change
-    add_column :pasiens, :reset_password_token, :string
-    add_column :pasiens, :reset_password_sent_at, :datetime
-  end
-end
