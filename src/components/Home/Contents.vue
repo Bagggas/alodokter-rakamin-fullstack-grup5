bd580182a158977618938f0263f69ca55d738813
Bagggas <abbbagas@gmail.com>
2021-12-15 23:29:37 +0700

    [Feat] Update

--- src/components/Home/Contents.vue
+++ src/components/Home/Contents.vue
@@ -1,11 +1,143 @@
 <template>
-  <h1>Anak Haram</h1>
-  <h1>Anak Haram</h1>
-  <h1>Anak Haram</h1>
-  <h1>Anak Haram</h1>
-  <h1>Anak Haram</h1>
-  <h1>Anak Haram</h1>
-  <h1>Anak Haram</h1>
-  <h1>Anak Haram</h1>
-  <h1>Anak Haram</h1>
+  <div class="container-fluid c-jumbotron">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="/css/Alo Asset/Banner.png" class="d-block w-100 img-carousel" >
+          <div class="carousel-caption d-none d-md-block text-dark">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="/css/Alo Asset/Banner.png" class="d-block w-100 img-carousel" >
+          <div class="carousel-caption d-none d-md-block text-dark">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="/css/Alo Asset/Banner.png" class="d-block w-100 img-carousel">
+          <div class="carousel-caption d-none d-md-block text-dark">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+  <div class="container mt-5 mb-5">    
+      <h1>Artikel terkini</h1>
+      <span>Memberikan informasi seputar kesehatan pilihan terbaru</span>
+      <hr>
+      <div class="row mb-3">
+        <div class="col-sm-4 mt-3">
+          <img src="/css/Alo Asset/Banner.png" class="card-img-top" alt="...">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 mt-3">
+          <img src="/css/Alo Asset/Banner.png" class="card-img-top" alt="...">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 mt-3">
+          <img src="/css/Alo Asset/Banner.png" class="card-img-top" alt="...">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 mt-3">
+          <img src="/css/Alo Asset/Banner.png" class="card-img-top" alt="...">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 mt-3">
+          <img src="/css/Alo Asset/Banner.png" class="card-img-top" alt="...">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 mt-3">
+          <img src="/css/Alo Asset/Banner.png" class="card-img-top" alt="...">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+  </div>
 </template>
+
+<style scoped>
+
+    .img-carousel{
+      opacity: 0.9;
+    }
+    h1{
+      color: #3c3e41;
+    }
+    .btn-primary{
+        background-color: #3570D2 !important;
+    }
+    
+    .bg-primary{
+        background-color: #dceaff !important;
+    }
+    .c-jumbotron{
+      padding: 0;
+      background-color: black;
+    }
+  .jumbotron-image {
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-primary text-light">
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright: Janjidokter
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+</template>
+
+<style scoped>
+  .navbar-light .navbar-toggler{
+    color: #ffff;
+    border-color: #ffff;
+  }
+    .bg-primary{
+        background-color: #3570D2 !important;
+    }
+
+    .navbar-dark .navbar-nav .nav-link{
+      color: #ffff !important;
+    }
+</style>
