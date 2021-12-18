ceb9618e1255687655db72cc9e365bc4e61ecad4
belladwir <belladwirh@gmail.com>
2021-12-18 18:25:01 +0700

    bella update

--- db/migrate/20211210102207_add_password_reset_columns_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordResetColumnsToUser < ActiveRecord::Migration[6.1]
-  def change
-    add_column :pasiens, :reset_password_token, :string
-    add_column :pasiens, :reset_password_sent_at, :datetime
-  end
-end
--- test/models/pasien_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class PasienTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
